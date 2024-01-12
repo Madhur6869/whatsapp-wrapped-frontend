7584e8b26a15cae0182ac63e8650f82965d7ab31
Madhur6869 <madhur.padavala2002@gmail.com>
2024-01-12 17:16:37 -0500

    Added steps and upload component on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import Head from "next/head";
+import Image from "next/image";
+
 import Navbar from "@/components/Navbar";
 import Steps from "@/components/Steps";
 
-import styles from "@/styles/home.module.scss"
+import styles from "@/styles/home.module.scss";
 
 export default function Home() {
   return (
@@ -12,13 +14,17 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <main className={styles.main}>
         <Navbar />
         <p className={styles.tagline}>
           Whatsapp Wrapped gives a summary <br />
           of your chats!
         </p>
-        <Steps/>
+        <Steps />
+        <section className={styles.upload}>
+          <Image className={styles.icon} src="/icons/upload.svg" height={128} width={128} />
+        <p className={styles.info}>Drag and drop or click to upload a file</p>
+        </section>
       </main>
     </>
   );
