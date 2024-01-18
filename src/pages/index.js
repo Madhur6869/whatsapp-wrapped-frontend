c24b248f12ddf9c04b94db3a995e6c209984192e
Madhur6869 <madhur.padavala2002@gmail.com>
2024-01-18 01:36:33 -0500

    Added drag and drop to upload, favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useState } from "react";
 
 import Navbar from "@/components/Navbar";
 import Steps from "@/components/Steps";
@@ -7,6 +8,25 @@ import Steps from "@/components/Steps";
 import styles from "@/styles/home.module.scss";
 
 export default function Home() {
+  const [uploadedFile, setUploadedFile] = useState(null);
+
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    setUploadedFile(file);
+    // API call fn
+  };
+
+  const handleDragOver = (event) => {
+    event.preventDefault();
+  };
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+    const file = event.dataTransfer.files[0];
+    setUploadedFile(file);
+    //API Call fn
+  };
+
   return (
     <>
       <Head>
@@ -20,10 +40,51 @@ export default function Home() {
           Whatsapp Wrapped gives a summary <br />
           of your chats!
         </p>
-        <Steps />
-        <section className={styles.upload}>
-          <Image className={styles.icon} src="/icons/upload.svg" height={128} width={128} />
-        <p className={styles.info}>Drag and drop or click to upload a file</p>
+        {!uploadedFile && <Steps />}
+        <section
+          className={styles.upload}
+          onDragOver={(event) => {
+            if (!uploadedFile) handleDragOver(event);
+          }}
+          onDrop={handleDrop}
+        >
+          {!uploadedFile && (
+            <>
+              <label htmlFor="fileInput">
+                <Image
+                  className={styles.icon}
+                  src="/icons/upload.svg"
+                  height={128}
+                  width={128}
+                />
+              </label>
+              <p className={styles.info}>
+                Drag and drop or click to upload a txt file
+              </p>
+              <input
+                type="file"
+                id="fileInput"
+                accept=".txt"
+                onChange={handleFileUpload}
+                style={{ display: "none" }}
+              />
+            </>
+          )}
+          {uploadedFile && (
+            <div className={styles.uploaded}>
+              <Image src="/icons/file.svg" width={200} height={200} />
+              <p className={styles.filename}>{uploadedFile.name}</p>
+              <button className={styles.submit}> 
+                Submit
+              </button>
+              <button
+                className={styles.remove}
+                onClick={() => setUploadedFile(null)}
+              >
+                Remove
+              </button>
+            </div>
+          )}
         </section>
       </main>
     </>
