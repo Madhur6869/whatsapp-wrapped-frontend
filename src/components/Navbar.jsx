98d8aef446355b73bb3c7690223fc3d993311018
Madhur6869 <madhur.padavala2002@gmail.com>
2024-03-29 16:03:31 -0400

    Minor design updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 import styles from "@/styles/components/navbar.module.scss";
 
 function Navbar() {
+  const router = useRouter()
   return (
     <div className={styles.navbar}>
       <div className={styles.main}>
         <Image
+        onClick={()=> {router.push("/")}}
           className={styles.logo}
           width={52}
           height={52}
